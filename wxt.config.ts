b95cfbb883091b44c11db45ca409f16fefa658b3
nnivxix <adhe.hanasa@gmail.com>
2026-08-18 12:58:33 +0700

    update version

--- wxt.config.ts
+++ wxt.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   manifest: {
     name: "YouTube CC Copy",
     description: "Copy YouTube closed-captions (CC) to clipboard",
-    version: "0.1.2",
+    version: "0.2.0",
     short_name: "YT CC Copy",
     permissions: ["storage", "tabs"],
     browser_specific_settings: {
